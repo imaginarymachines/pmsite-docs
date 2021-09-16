cf236b618d6a183d3108e67c6555115ba57865e6
Josh Pollock <jpollock412@gmail.com>
2021-09-16 18:16:50 -0400

    Shortversion block

--- blocks/help-short-version/Edit.js
+++ blocks/help-short-version/Edit.js
@@ -1,26 +1,49 @@
-import { TextControl } from "@wordpress/components";
 import { __ } from "@wordpress/i18n";
-import { useBlockProps } from "@wordpress/block-editor";
-import './editor.scss';
+import {
+	useBlockProps,
+	RichText,
+	AlignmentToolbar,
+	BlockControls,
+} from "@wordpress/block-editor";
+import "./editor.scss";
 
-export const Editor = ({ value, onChange, isSelected }) => (
-	<>
-		{isSelected ? (
-			<TextControl value={value} onChange={onChange} />
-		) : (
-			<p>{value}</p>
-		)}
-	</>
-);
+export const Editor = ({ attributes, setAttributes }) => {
+	const onChangeContent = (newContent) => {
+		setAttributes({ content: newContent });
+	};
 
-export default function Edit({ attributes, setAttributes, isSelected }) {
+	const onChangeAlignment = (newAlignment) => {
+		setAttributes({
+			alignment: newAlignment === undefined ? "none" : newAlignment,
+		});
+	};
+
 	return (
 		<div {...useBlockProps()}>
-			<Editor
-				isSelected={isSelected}
+			<h2>{__("Short Version")}</h2>
+			{
+				<BlockControls>
+					<AlignmentToolbar
+						value={attributes.alignment}
+						onChange={onChangeAlignment}
+					/>
+				</BlockControls>
+			}
+			<RichText
+				className={attributes.className}
+				style={{ textAlign: attributes.alignment }}
+				tagName="p"
+				onChange={onChangeContent}
 				value={attributes.content}
-				onChange={(content) => setAttributes({ content })}
 			/>
+		</div>
+	);
+};
+
+export default function Edit(props) {
+	return (
+		<div {...useBlockProps()}>
+			<Editor {...props} />
 		</div>
 	);
 }
--- blocks/helpful-links/Edit.js
+++ blocks/helpful-links/Edit.js
@@ -4,7 +4,7 @@ import { useBlockProps, InnerBlocks } from "@wordpress/block-editor";
 import "./editor.scss";
 const ALLOWED_BLOCKS = ["pmsite-docs/help-link"];
 
-export const Above = () => <h3>{__("Helpful Links")}</h3>;
+export const Above = () => <h2>{__("Helpful Links")}</h2>;
 
 export default function Edit({ attributes, setAttributes, isSelected }) {
 	return (
