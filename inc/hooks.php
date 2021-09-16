0a0dc968a6207b097332af7f2610a49502ab50b3
Josh Pollock <jpollock412@gmail.com>
2021-09-16 18:32:14 -0400

    Register extra REST API fields

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,2 +1,52 @@
-<?php 
-/** Actions and Filters **/
+<?php
+
+add_action(
+    'rest_api_init',
+    function () {
+        register_rest_field(
+            'doc',
+            'helpLinks',
+           [
+                'get_callback'    => function ( $object ) {
+                    $links = [];
+                    $parsed = parse_blocks( $object['content']['raw']);
+                    foreach($parsed as $block){
+                        if( 'pmsite-docs/helpful-links' === $block['blockName']){
+                            foreach ($block['innerBlocks'] as $innerBlock) {
+                                if( 'pmsite-docs/help-link' === $innerBlock['blockName']){
+                                    $links[] = $innerBlock['attrs'];
+                                }
+                            }
+                        }
+                    }
+                    return $links;
+                },
+                'update_callback' => null,
+                'schema'          => [
+                    'type'        => 'array',
+                ],
+           ]
+        );
+        register_rest_field(
+            'doc',
+            'shortVersion',
+           [
+                'get_callback'    => function ( $object ) {
+                    $theBlock = [];
+                    $parsed = parse_blocks( $object['content']['raw']);
+
+                    foreach($parsed as $block){
+                        if( 'pmsite-docs/help-short-version' === $block['blockName']){
+                           $theBlock = $block;
+                        }
+                    }
+                    return  $theBlock;
+                },
+                'update_callback' => null,
+                'schema'          => [
+                    'type'        => 'array',
+                ],
+           ]
+        );
+    }
+);
