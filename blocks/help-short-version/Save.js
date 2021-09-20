7a359591ef05e1f51e3c31623eefe46fdae46d7b
Josh Pollock <jpollock412@gmail.com>
2021-09-20 13:38:06 -0400

    fix zipmaker

--- blocks/help-short-version/Save.js
+++ blocks/help-short-version/Save.js
@@ -1,6 +1,20 @@
-import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
-
+import { __ } from "@wordpress/i18n";
+import { useBlockProps } from "@wordpress/block-editor";
+import { createElement } from "@wordpress/element";
 export default function save({ attributes }) {
-  return <div {...useBlockProps.save()}>{attributes.content}</div>;
+	if (attributes.content && attributes.content.length > 0) {
+		return (
+			<div {...useBlockProps.save()}>
+				{attributes.content.map((c) => {
+					if ("string" === typeof c) {
+						return <p>{c}</p>;
+					} else if (c.hasOwnProperty("type")) {
+						return createElement(c.type, c.props, c.props.children);
+					}
+					return null;
+				})}
+			</div>
+		);
+	}
+	return <div {...useBlockProps.save()} />;
 }
--- makeZip.js
+++ makeZip.js
@@ -1,25 +1,33 @@
-var fs = require('fs');
-var archiver = require('archiver');
+var fs = require("fs");
+var archiver = require("archiver");
 
-var output = fs.createWriteStream('Plugin Machine Docs.zip');
-var archive = archiver('zip');
+var output = fs.createWriteStream("pmsite-docs.zip");
+var archive = archiver("zip");
 
-console.log( 'Zipping!')
-output.on('close', function () {
-    console.log('Zipped!');
-    console.log(archive.pointer() + ' total bytes');
+console.log("Zipping!");
+output.on("close", function () {
+	console.log("Zipped!");
+	console.log(archive.pointer() + " total bytes");
 });
 
-archive.on('error', function(err){
-    throw err;
+archive.on("error", function (err) {
+	throw err;
 });
 
 archive.pipe(output);
 
-archive.append(fs.createReadStream(
-    __dirname + '/Plugin Machine Docs.php'
-), { name: 'Plugin Machine Docs.php' });
+["admin", "php", "inc", "blocks", "vendor", "build"].forEach((dir) => {
+	if (fs.existsSync(`${__dirname}/${dir}`)) {
+		archive.directory(`${dir}/`, dir);
+	}
+});
 
-archive.directory('build', '/build');
+["pmsite-docs.php", "readme.txt"].forEach((name) => {
+	archive.append(fs.createReadStream(`${__dirname}/${name}`), {
+		name,
+	});
+});
+
+archive.directory("build", "/build");
 
 archive.finalize();
